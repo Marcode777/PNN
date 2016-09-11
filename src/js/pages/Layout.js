b9eb6d208a74cb7bae761796c0b2c19d67f3bfd8
Marcode777 <marcosebello7@gmail.com>
2016-09-11 15:34:24 -0400

    modified src/js/pages/Layout.js, implemented fullpage background image

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -8,11 +8,20 @@ import Archives from "./Archives";
 
 import {Link} from "react-router";
 
+  var jsStyle = {
+  backgroundImage: 'url("https://s3.amazonaws.com/s3.imagefinder.co/uploads/2016/01/20143942/boss-fight-free-high-quality-stock-images-photos-photography-new-york-city-skyline-960x640.jpg")',
+  position:"absolute",
+  WebkitBackgroundSize: 'cover', // note the capital 'W' here
+  height:"100%",
+  width:"100%",
+  // WebkitTransition: 'all', // note the capital 'W' here
+  // msTransition: 'all' // 'ms' is the only lowercase vendor prefix
+};
+
 export default class Layout extends React.Component {
   render() {
     return (
-      <div>
-        <img src={"http://www.funonsite.com/funarea/wallpapers/Good-Sunrise-hd_wallpaper_free_download.jpg"}/>
+      <div style={jsStyle}>
         <h1>Your Source For Uplifting News (because mainstream is just too negative)</h1>
         {this.props.children}
         <Link to="archives"><button class="btn btn-primary">archives</button></Link>
