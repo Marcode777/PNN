6512f59a78a523a7aeb89995e99a794e58e79bbb
Marcode777 <marcosebello7@gmail.com>
2016-09-19 06:38:27 -0400

    recalibrated for prelaunch, removed Procfile, removed server.js, modified package.json

--- server.js
+++
@@ -1,5 +0,0 @@
-var express = require('express'); 
-var app = express(); 
-app.use("/", express.static(__dirname + '/src')); 
-app.get('*', function(req, res){ res.sendFile(__dirname + '/src/PNN.html')}); 
-app.listen(process.env.PORT || 8080);
